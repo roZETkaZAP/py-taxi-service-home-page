4580fa49a6f9a21c6da6beedf09d29ad7a81cad2
Igor <zalugin.iv2@gmail.com>
2026-07-16 17:49:49 +0200

    flake8 changes added

--- taxi/views.py
+++ taxi/views.py
@@ -7,5 +7,9 @@ def index(request):
     num_drivers = Driver.objects.count()
     num_manufacturers = Manufacturer.objects.count()
     num_cars = Car.objects.count()
-    context = {"num_drivers": num_drivers, "num_manufacturers": num_manufacturers, "num_cars": num_cars}
-    return render(request, "taxi/index.html", context)
+    context = {"num_drivers": num_drivers,
+               "num_manufacturers": num_manufacturers,
+               "num_cars": num_cars
+               }
+    return render(request,
+                  "taxi/index.html", context)
